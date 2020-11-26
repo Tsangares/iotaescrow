bd4cd2192ea921839802d217451117407f28b456
tsangares <tsangares@gmail.com>
2020-11-26 10:56:19 +0000

    escrow

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .escrow import Escrow
--- escrow.py
+++ escrow.py
@@ -1,35 +1,73 @@
 import time,json,os,subprocess,requests
 from iota import Iota, ProposedTransaction, Address, TryteString, Fragment, Transaction,adapter,ProposedBundle
 from iota.crypto.addresses import AddressGenerator
-
+import pathlib,logger
 class Escrow:
     def __init__(self,node='https://nodes.thetangle.org:443'):
+        #Get Seet
         self.seed = self.getSeed()
+
+        #Setup API
         self.api = Iota(node,self.seed)
 
-    #Generates a seed to act as the escrow
+    #Generates a seed for escrow account
     def getSeed(self):
+        #If no seed, create one
         if not os.path.isfile('seed.txt'):
-            subprocess.run(['./generateSeed'])
-            print("Placed new seed in seed.txt")
+            path = pathlib.Path(__file__).parent.absolute()
+            subprocess.run([f'{path}/generateSeed'])
+            logger.info("Placed new seed in seed.txt")
         return open('seed.txt').read().strip().encode('utf-8')
     
-    #Creates an escrow process
-    def create(self,collateral,fee=0,deposit=None):
-        self.fee=0
-        if self.requestDeposit(collateral,deposit):
+    #Creates an escrow holding address
+    def createEscrow(self):
+        self.holdingAddress = self.api.get_new_addresses(count=None,checksum=True)['addresses'][0]
+        return self.holdingAddress
+
+    #Waits for a transactions with a refund address
+    def getRefundAddress(self):
+        #This is the escrow address
+        address = self.holdingAddress
+        try:
+            #Get Hashes from ledger
+            txHashes = self.api.find_transactions(addresses=[address,])['hashes']
+            #If no hashes, user has not submitted an address yet.
+            if len(txHashes)==0:
+                return None
+            else:
+                #Check messages for a valid address
+                txs = self.api.get_transaction_objects(txHashes)['transactions']  
+                for tx in txs:
+                    msg = tx.signature_message_fragment.decode()
+                    try:
+                        return Address(msg.strip())
+                    except: pass
+                logger.warning("Invalid address recieved")
+        except requests.exceptions.ConnectionError as e:
+            #Sometimes the public nodes will reject a request
+            print("Error contacting server; retrying")
+            return self.getRefundAddress()
+
+    #Cli version of escrow
+    def startCli(self,collateral,fee=0,delay=120,deposit=None):
+        #Create holding address
+        self.createEscrow()
+        #Wait for a deposit address to be entered
+        if self.requestDeposit(collateral,deposit,delay):
             while not self.checkCondition():
                 sleep(3)
         self.finalizeEscrow()
                 
-    
-    def requestDeposit(self,collateral,deposit=None):
+    #Wait for escrow address to recieve collateral
+    def requestDeposit(self,collateral,deposit=None,duration=120):
+        #For CLI prompt a deposit address
         if deposit is None:
             self.deposit = input("What is the deposit address: ")
-        self.holdingAddress = self.api.get_new_addresses(count=None,checksum=True)['addresses'][0]
-        print(f"You have 1 min to deposit {collateral} MIOTA to {self.holdingAddress}")
+        print(f"You have {duration/60:.1f} min to deposit {collateral} MIOTA to {self.holdingAddress}")
+
+        #Wait for escrow to recive collateral funds.
         count = 0
-        while count < 60:
+        while count < duration:
             time.sleep(1)
             balance = self.getBalance(self.holdingAddress)
             if balance >= collateral:
@@ -37,19 +75,23 @@ class Escrow:
                 return True
         return False
 
-
+    #Condition to release escrow
     def checkCondition(self):
-        #Go to ledger and check for condition.
-        #Use streams?
+        #Setup a check condition
+        #For example RFID or some ledger condition
         return True
 
+    #Refund user their collateral, remoing the fee
     def finalizeEscrow(self):
         #Return money to deposit address
         returnAmount=self.getBalance(self.holdingAddress)
+        
+        #Calcualte return amount
         if returnAmount > 0:
             returnAmount -= self.fee
-            
-        message="Repayment testing."
+
+        #Setup transaction
+        message="Repayment of collateral"
         feeLocation = self.api.get_new_addresses(count=1,checksum=True)['addresses'][0]
         txs = [
             ProposedTransaction(
@@ -58,10 +100,11 @@ class Escrow:
                 message = TryteString.from_unicode(message)
             ),
         ]
-        inputs = [Address(self.holdingAddress),]
+        
+        #Send transaction
         bundle = self.api.send_transfer(transfers=txs)['bundle']
-        print(bundle.transactions[0].hash)
-        print("Sent money back to recipient")
+        logger.info(bundle.transactions[0].hash)
+        logger.info("Sent money back to recipient")
         self.addRevenue(fee)
 
     def getBalance(self,address):
@@ -69,19 +112,23 @@ class Escrow:
             response = self.api.get_balances(addresses=[address])['balances']
             return response[0]
         except requests.exceptions.ConnectionError as e:
-            print("Error contacting server; retrying")
+            logger.info("Error contacting server; retrying")
             return getBalance(self,address)
-        
-    def addRevenue(self,money,filename='profits.txt'):
+
+    #Record the amount of revenue recieved
+    def addRevenue(self,money,filename='revenue.txt'):
         if not os.path.isfile(filename):
-            open('profits.txt','w+').write('0')
+            open(filename,'w+').write('0')
         current = int(open(filename).read().strip())
         current+=money
         open(filename).write(current)
-    def getRevenue(self,filename="profits.txt"):
+
+    #Get the current amount of revenue
+    def getRevenue(self,filename="revenue.txt"):
         if not os.path.isfile(filename): return 0
         return int(open(filename).read().strip())
-    
+
+    #Send revenue to an address
     def sendRevenue(self,outputAddress):
         revenue = self.getRevenue()
         message="Output fees from escrow."
@@ -96,8 +143,8 @@ class Escrow:
         print(bundle.transactions[0].hash)
 
 def createEscrow(args):
-    escrow = Escrow()
-    escrow.create(50,7,node=args.node)
+    escrow = Escrow(node=args.node)
+    escrow.startCli(50,7)
     
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Basic escrow using IOTA.')
